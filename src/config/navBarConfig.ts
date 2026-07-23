f0b42c86ab85f6f41cd09c5e8e3edfb07a3d30b5
ykfy1 <yktcyfy@foxmail.com>
2026-07-23 19:52:19 +0800

    Update navBarConfig.ts

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -53,9 +53,6 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		url: "#",
 		icon: "material-symbols:person",
 		children: [
-			// 动态
-			LinkPresets.Dynamic,
-
 			// 相册
 			LinkPresets.Gallery,
 
@@ -66,6 +63,46 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 			LinkPresets.Bangumi,
 		],
 	});
+
+	// 自定义导航栏链接
+	links.push({
+		name: "链接",
+		url: "#",
+		icon: "material-symbols:link",
+		// 子菜单
+		children: [
+			{
+				name: "GitHub",
+				url: "https://github.com/",
+				external: true,
+				icon: "fa7-brands:github",
+			},
+			{
+				name: "IPTV",
+				url: "https://blog.yangfeiyue.bot.cd/posts/iptv/",
+				external: true,
+				icon: "fa7-solid:tv",
+			},
+			{
+				name: "音乐播放器",
+				url: "https://music.yangfeiyue.bot.cd/",
+				external: true,
+				icon: "fa7-solid:music",
+			},
+			{
+				name: "烟花",
+				url: "https://yanhua.yangfeiyue.bot.cd/",
+				external: true,
+				icon: "fa7-solid:burst",
+			},
+		],
+	});
+
+	// 友链
+	links.push(LinkPresets.Friends);
+
+	// 留言板
+	links.push(LinkPresets.Guestbook);
 
 	// 关于及其子菜单
 	links.push({
@@ -81,44 +118,10 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		],
 	});
 
-	// 自定义导航栏链接
-	links.push({
-		name: "链接",
-		url: "#",
-		icon: "material-symbols:link",
-		// 子菜单
-		children: [
-			{
-				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:github",
-			},
-			{
-				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:gitee",
-			},
-			{
-				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
-				external: true,
-				icon: "fa7-brands:qq",
-			},
-			{
-				name: "Firefly文档",
-				url: "https://docs-firefly.cuteleaf.cn",
-				external: true,
-				icon: "material-symbols:docs",
-			},
-		],
-	});
-
 	// 文档链接
 	// links.push({
 	// 	name: "文档",
-	// 	url: "https://docs-firefly.cuteleaf.cn",
+	// 	url: "https://blog.yangfeiyue.bot.cd/",
 	// 	external: true,
 	// 	icon: "material-symbols:docs",
 	// });
